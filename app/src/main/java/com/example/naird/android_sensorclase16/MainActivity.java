d361225cf6d8d96856ffcd149c022158fee00a75
addyyarci <adi.fuente3@gmail.com>
2016-06-19 00:16:41 -0500

    version 20160619_1206am

--- app/src/main/java/com/example/naird/android_sensorclase16/MainActivity.java
+++ app/src/main/java/com/example/naird/android_sensorclase16/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.naird.android_sensorclase16;
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    LinearLayout ln;
+    SensorManager sm;
+    Sensor sensor;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ln = (LinearLayout) findViewById(R.id.linear);
+        tv = (TextView) findViewById(R.id.valorText);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        sm.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        String texto = String.valueOf(event.values[0]);
+        tv.setText(texto);
+
+        float valor = Float.parseFloat(texto);
+
+        if(valor == 0){
+            ln.setBackgroundColor(Color.BLUE);
+        }
+        else{
+            ln.setBackgroundColor(Color.YELLOW);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
